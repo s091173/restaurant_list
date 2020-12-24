77f109ad9a609ae2a00bace9de1ee0a83d23d10e
Yuchi Su <astraiostor@gmail.com>
2020-12-24 22:38:31 +0800

    feat: add delete restaurant function

--- app.js
+++ app.js
@@ -98,6 +98,15 @@ app.post('/restaurants/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// Delete 路由
+app.get('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 // start and listen on the Express server
 app.listen(port, () => {
   console.log(`The server is running on http://localhost:${port}`)
