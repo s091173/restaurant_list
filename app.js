622aa50e6153e2a265b701e837603407ba25c61b
Yuchi Su <astraiostor@gmail.com>
2021-03-14 16:44:15 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // require packages used in the project
 const express = require('express')
-
+// 載入 express-session
+const session = require('session')
 // 載入 express-handlebars 
 const exphbs = require('express-handlebars')
 // 載入 bodyParser
@@ -22,6 +23,12 @@ const port = 3000
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 // setting static files
 app.use(
