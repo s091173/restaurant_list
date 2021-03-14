0de0df5651459ec95a92471f8b0f39fec75d38ea
Yuchi Su <astraiostor@gmail.com>
2021-03-14 14:47:59 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,17 @@ const router = express.Router()
 
 // 引用 home 模組程式碼
 const home = require('./modules/home')
-// 引用 restaurant 模組程式碼
+// 引用 restaurants 模組程式碼
 const restaurants = require('./modules/restaurants')
+// 引用 users 模組程式碼
+const users = require('./modules/users')
 
 // 將網址結構符合 / 字串的 request 導向 home 模組 
 router.use('/', home)
 // 將網址結構符合 /restaurants 字串的 request 導向 restaurants 模組
 router.use('/restaurants', restaurants)
+// 將網址結構符合 /users 字串的 request 導向 users 模組
+router.use('/users', users)
 
 // 匯出路由器
 module.exports = router
---
+++ routes/modules/users.js
@@ -0,0 +1,11 @@
+// users 路由模組
+
+const express = require('express')
+const router = express.Router()
+
+// login page
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+module.exports = router
